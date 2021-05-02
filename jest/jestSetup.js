5ad893cc6fcac860463493d3eb88f35affa87f50
hbiede <6586509+hbiede@users.noreply.github.com>
2021-05-02 14:11:19 -0500

    Give up on automated push to itunes and remove Jest

--- jest/jestSetup.js
+++
@@ -1,4 +0,0 @@
-import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
-
-// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
