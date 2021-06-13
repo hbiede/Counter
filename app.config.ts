96ecc4c7d881689b740b64c5f349d224d7377eb4
hbiede <6586509+hbiede@users.noreply.github.com>
2021-06-13 00:50:18 -0500

    Publish Version 1.2.2

--- app.config.ts
+++ app.config.ts
@@ -10,7 +10,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   entryPoint: 'src/App.tsx',
   privacy: 'public',
   platforms: ['ios', 'android'],
-  version: '1.2.1',
+  version: '1.2.2',
   orientation: 'default',
   icon: './assets/icon.png',
   splash: {
@@ -28,7 +28,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   android: {
     package: 'com.hbiede.Counter',
-    versionCode: 10201,
+    versionCode: 10202,
     softwareKeyboardLayoutMode: 'resize',
   },
 });
