ce407e2e90b1253e469a762e9bc62728774d3206
Hundter Biede <6586509+hbiede@users.noreply.github.com>
2022-03-27 20:22:11 -0500

    Convert to EAS Build

---
+++ index.js
@@ -0,0 +1,6 @@
+// Required due to EAS not supporting arbitrary entry points
+import { registerRootComponent } from 'expo';
+
+import App from './src/App';
+
+registerRootComponent(App);
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { registerRootComponent } from 'expo';
 import {
   lockAsync,
   OrientationLock,
@@ -44,5 +43,4 @@ const App = (): JSX.Element => {
   );
 };
 
-registerRootComponent(App);
 export default App;
